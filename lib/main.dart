55306160573f151c0f3222a1bf7606dc744fa92d
suprim12 <suprimofficial@yahoo.com>
2021-02-12 09:59:48 +0545

    All Changes

--- lib/main.dart
+++ lib/main.dart
@@ -8,8 +8,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'Bloc App',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
+        primarySwatch: Colors.black,
         accentColor: Colors.black
       ),
       initialRoute: AppRouter.home,
